69624df80eda359143870d96a0de05bf93de3511
renanjava <renancvr777@gmail.com>
2025-09-15 19:48:02 -0300

    feat: adding prisma service and resource for curso and disciplina

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CursoModule } from './curso/curso.module';
+import { DisciplinaModule } from './disciplina/disciplina.module';
 
 @Module({
-  imports: [],
+  imports: [CursoModule, DisciplinaModule],
   controllers: [AppController],
   providers: [AppService],
 })
