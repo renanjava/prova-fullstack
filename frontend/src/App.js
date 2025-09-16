ed31f6df1eaf4bb1f018b3fcad84b09d36ad9db9
renanjava <renancvr777@gmail.com>
2025-09-15 21:28:09 -0300

    feat: adding F.E with get API data and delete button for Curso

---
+++ frontend/src/App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [cursoData, setCurso] = useState([])
+  const [disciplinaData, setDisciplina] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/curso")
+      .then(response => response.json())
+      .then(data => setCurso(data))
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/disciplina")
+      .then(response => response.json())
+      .then(data => setDisciplina(data))
+  }, [])
+
+  return (
+    <div className="App">
+      <h2>Lista de Cursos</h2>
+            <ul>
+                {cursoData.map(curso => (
+                    <li key={curso.id}>
+                        <button onClick={() => {
+                          fetch("http://localhost:3000/curso/"+curso.id, {
+                            method: 'DELETE'
+                          })
+                          .then(res => res.text())
+                          .then(res => console.log(res))
+                        }}>Delete</button>
+                        <p>
+                            Nome:
+                            {curso.nome}
+                        </p>
+                        <p>
+                            Carga Horária:
+                            {curso.cargaHoraria}
+                        </p>
+                        <p>
+                            Data Início:
+                            {curso.dataInicio}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+          <h2>Lista de Disciplinas</h2>
+            <ul>
+                {disciplinaData.map(disciplina => (
+                    <li key={disciplina.id}>
+                        <p>
+                            Nome:
+                            {disciplina.nome}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+    </div>
+  );
+}
+
+export default App;
